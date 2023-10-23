0ccbc0258b00cf364945f4d5457f06d4e63d344f
Michal Korman <michal.korman@icloud.com>
2023-10-23 21:44:22 +0200

    add apple asset

--- src/scenes/Example.ts
+++ src/scenes/Example.ts
@@ -5,6 +5,7 @@ export class Example extends Phaser.Scene {
     this.load.image('starfield', '/assets/starfield.jpg');
     this.load.image('logo', '/assets/phaser3-logo.png');
     this.load.image('red', '/assets/red.png');
+    this.load.image('apple', '/assets/apple.png');
   }
 
   create() {
@@ -16,16 +17,16 @@ export class Example extends Phaser.Scene {
       'starfield'
     );
 
+    this.add.image(this.scale.width / 2, this.scale.height / 2, 'logo')
+      .setScale(0.7, 0.7);
+
     const particles = this.add.particles(0, 0, 'red', {
       speed: 100,
-      scale: { start: 1, end: 0 },
+      scale: { start: 0.5, end: 0 },
       blendMode: 'ADD'
     });
 
-    this.add.image(this.scale.width / 2, this.scale.height / 2, 'logo');
-
-    const logo = this.physics.add.image(100, 100, 'logo');
-    logo.setScale(0.5, 0.5);
+    const logo = this.physics.add.image(100, 100, 'apple');
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
